23aaa42ab419765ab8ed783cbe8e2a8b660b0d1f
explorer-man321 <vanluc3223@gmail.com>
2022-07-14 15:37:36 +0700

    my work desc

---
+++ app.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql");
+const bodyparser = require("body-parser");
+const express = require("express");
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+const poolconnection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'question_db'
+});
+
+app.get('/getquestion/:id', (req, res) => {
+    poolconnection.getConnection((err, connection) => {
+        if (err) throw err;
+        let sql = `SELECT question FROM question_table WHERE id = ${req.params.id}`;
+        connection.query(sql, (err, rows) => {
+            if (err) throw err;
+            res.send(rows);
+        })
+    })
+})
+
+app.listen(port, () => {
+    console.log(`${port}`);
+})
